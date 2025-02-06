7cf6b0be814b9b834d365364f62c62be4072421b
jurijreact <f888@gmail.com>
2025-02-06 12:39:09 +0200

    111

--- src/core/cats/CatsList.js
+++ src/core/cats/CatsList.js
@@ -1,4 +1,6 @@
+
 import {useDispatch, useSelector} from "react-redux";
+import React from "react";
 import {useEffect} from "react";
 import {catsActions} from "./catSlice";
 import {dividerClasses} from "@mui/material";
@@ -17,9 +19,11 @@ const CatsList = () => {
     return(
         <div style={{display:'flex', flexDirection:'column'}}>
             <div>CatsList</div>
-            <div>{(isLoading)?'Loading...':'isLoading = false'}</div>
+            <div>{(isLoading)?'Loading...':'Data OK'}</div>
             {(0!==catsState?.cats.length) && catsState.cats.map((el,ii)=>{
-                return <div key={ii}>{el.name}</div>
+                return <React.Fragment key={ii}>
+                    <div >{el.id} ---  {el.name}</div>
+                </React.Fragment>
             })}
             {/*<div>{JSON.stringify(catsState)}</div>*/}
         </div>
