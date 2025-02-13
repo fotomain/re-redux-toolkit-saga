d020c1068a2956cba3153c147c8bb1f5dd59cf9a
jurijreact <f888@gmail.com>
2025-02-13 16:29:58 +0200

    111

--- src/core/cats/catSagaCreate.js
+++ src/core/cats/catSagaCreate.js
@@ -1,22 +1,23 @@
 
-import {fork, call, put, takeEvery} from "redux-saga/effects";
+import {delay, fork, call, put, takeEvery} from "redux-saga/effects";
+
 import {catsActions} from "./catSlice";
 
 
 const slow_function = async (params) => {
-    let c=0;
 
-    const apiResponse0 = await fetch("https://api.thecatapi.com/v1/breeds")
-    for (let i = 0; i < 50 ; i++) {
-        const apiResponse0 = await fetch("https://api.thecatapi.com/v1/breeds")
-        console.log('loop1',i)
-    }
-    // for (let i = 0; i < 1000_000_000; i++) {c++}
+    const apiResponse0 = await fetch("fake fetch api");
+    let c=0;
+    console.log('=== delay2 start')
+    console.log('=== delay2 input not working')
+    for (let i = 0; i < 10000_000_000; i++) {c++}
+    console.log('=== delay2 finish')
     console.log('=== c',c)
 
 };
 const apiExecute = async (params) => {
     // === !!! await always needed
+    // const apiResponse0 = await fetch("fake fetch api");
 
     const res = await slow_function(params)
 
@@ -25,14 +26,14 @@ const apiExecute = async (params) => {
 }
 
 function* workFetch(params){
-    console.log('params.payload1',params.payload)
-    // const cats = yield call(()=>fetch("https://api.thecatapi.com/v1/breeds"));
-    //=== TIMEOUT 3s 3000_000_000
-    // console.log('call catsActions.setcreateInProcess')
-    // yield put(catsActions.setcreateInProcess({...params.payload }))
-    console.log('params.scope1',params.payload.scope)
+    // console.log('params.payload1',params.payload)
 
     yield put(catsActions.catCreateStart(params))
+
+    console.log('=== delay1 start')
+    console.log('=== delay1 input working OK')
+    yield delay(4000)
+    console.log('=== delay1 finish')
 
     const apiResponse = yield call(()=>apiExecute(params));
 
